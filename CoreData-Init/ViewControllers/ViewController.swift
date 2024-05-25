bee5196aa3856afba212de23ac7007e7e2a2b94d
Gokhan Mutlu <gokhanmutlu04@yahoo.com>
2024-05-25 19:11:30 +0300

    add initCoreDataAndPrintViewContext function in ViewController

--- CoreData-Init/ViewControllers/ViewController.swift
+++ CoreData-Init/ViewControllers/ViewController.swift
@@ -6,10 +6,19 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
 		print("ViewController.\(#function)")
+		
+		initCoreDataAndPrintViewContext()
 	}
 
-
+	private func initCoreDataAndPrintViewContext(){
+		print(String(repeating: "---- ", count: 5))
+		let coredataManager = CoreDataManager()
+		let context = coredataManager.viewContext
+		print(context)
+		
+		print("Core Data - ViewContext created\nHappy Coding 🐝")
+		print(String(repeating: "---- ", count: 5))
+	}
 }
 
