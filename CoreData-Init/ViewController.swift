eba282f0d6f1d268a8bd061f7c9bc51db436a9de
Gokhan Mutlu <gokhanmutlu04@yahoo.com>
2024-05-25 13:23:09 +0300

    Initial Commit

---
+++ CoreData-Init/ViewController.swift
@@ -0,0 +1,14 @@
+// ViewController.swift by Gokhan Mutlu on 25.05.2024
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
